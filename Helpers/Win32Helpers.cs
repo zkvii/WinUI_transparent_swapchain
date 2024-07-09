2c849a209be4d2f1af22e70cb52a78dd5a3751d0
LiuKai <2018282110208@whu.edu.cn>
2024-07-10 01:45:04 +0800

    set wallpaper

--- Helpers/Win32Helpers.cs
+++ Helpers/Win32Helpers.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SharpGen.Runtime;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -9,6 +10,71 @@ namespace FluentCountDown.Helpers;
 
 static class Win32Helpers
 {
+
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern IntPtr SetParent(IntPtr child, IntPtr parent);
+
+
+    [DllImport("user32.dll")]
+    public static extern IntPtr GetDesktopWindow();
+
+    [Flags]
+    public enum SendMessageTimeoutFlags : uint
+    {
+        SMTO_NORMAL = 0x0000,
+        SMTO_BLOCK = 0x0001,
+        SMTO_ABORTIFHUNG = 0x0002,
+        SMTO_NOTIMEOUTIFNOTHUNG = 0x0008,
+        SMTO_ERRORONEXIT = 0x0020
+    }
+
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern IntPtr SendMessageTimeout(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam, SendMessageTimeoutFlags fuFlags, uint uTimeout, out IntPtr lpdwResult);
+
+    public enum DWMWINDOWATTRIBUTE
+    {
+        DWMWA_NCRENDERING_ENABLED = 1,
+        DWMWA_NCRENDERING_POLICY,
+        DWMWA_TRANSITIONS_FORCEDISABLED,
+        DWMWA_ALLOW_NCPAINT,
+        DWMWA_CAPTION_BUTTON_BOUNDS,
+        DWMWA_NONCLIENT_RTL_LAYOUT,
+        DWMWA_FORCE_ICONIC_REPRESENTATION,
+        DWMWA_FLIP3D_POLICY,
+        DWMWA_EXTENDED_FRAME_BOUNDS,
+        DWMWA_HAS_ICONIC_BITMAP,
+        DWMWA_DISALLOW_PEEK,
+        DWMWA_EXCLUDED_FROM_PEEK,
+        DWMWA_CLOAK,
+        DWMWA_CLOAKED,
+        DWMWA_FREEZE_REPRESENTATION,
+        DWMWA_PASSIVE_UPDATE_MODE,
+        DWMWA_USE_HOSTBACKDROPBRUSH,
+        DWMWA_USE_IMMERSIVE_DARK_MODE = 20,
+        DWMWA_WINDOW_CORNER_PREFERENCE = 33,
+        DWMWA_BORDER_COLOR,
+        DWMWA_CAPTION_COLOR,
+        DWMWA_TEXT_COLOR,
+        DWMWA_VISIBLE_FRAME_BORDER_THICKNESS,
+        DWMWA_SYSTEMBACKDROP_TYPE,
+        DWMWA_LAST
+    };
+
+    public enum DWM_WINDOW_CORNER_PREFERENCE
+    {
+        DWMWCP_DEFAULT = 0,
+        DWMWCP_DONOTROUND = 1,
+        DWMWCP_ROUND = 2,
+        DWMWCP_ROUNDSMALL = 3
+    }
+
+    [DllImport("Dwmapi.dll", SetLastError = true, CharSet = CharSet.Unicode, PreserveSig = false)]
+    public static extern IntPtr DwmSetWindowAttribute(IntPtr hwnd, DWMWINDOWATTRIBUTE dwAttribute, ref DWM_WINDOW_CORNER_PREFERENCE pvAttribute, uint cbAttribute);
+
+    [DllImport("Dwmapi.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+    public static extern IntPtr DwmSetWindowAttribute(IntPtr hwnd, int dwAttribute, ref int pvAttribute, int cbAttribute);
+
+
 
     [DllImport("User32.dll", SetLastError = true, CharSet = CharSet.Auto)]
     public static extern bool RedrawWindow(IntPtr hWnd, IntPtr lprcUpdate, IntPtr hrgnUpdate, uint flags);
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,49 +17,49 @@ using Vortice.Mathematics;
 using Vortice.WIC;
 using FeatureLevel = Vortice.Direct3D.FeatureLevel;
 using FluentCountDown.Helpers;
+using System.Runtime.InteropServices;
+using Microsoft.UI.Windowing;
+using Microsoft.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.UI.WindowsAndMessaging;
+using MathNet.Numerics;
+using static FluentCountDown.Helpers.Win32Helpers;
 
 namespace FluentCountDown;
-
-public struct DLinePoint(float x, float y, float pressure)
-{
-    public float X = x;
-    public float Y = y;
-    public float Pressure = pressure;
-}
 
 public sealed partial class MainWindow
 {
     private DispatcherTimer timer;
 
     private IntPtr mHwnd;
+    private Microsoft.UI.Windowing.OverlappedPresenter _presenter;
 
     public MainWindow()
     {
-        //easy way to remain system title bar
-        ExtendsContentIntoTitleBar = true;
-
         InitializeComponent();
-        
+
+        //easy way to remain system title bar
 
         mHwnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
 
-        AppWindow.MoveAndResize(new RectInt32(100,100,400,600));
-
+        // AppWindow.SetPresenter(AppWindowPresenterKind.FullScreen);
         // Width = 600;
         // Height = 400;
-        // var winId= Microsoft.UI.Win32Interop.GetWindowIdFromWindow(mHwnd);
-        // var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(winId);
+        var winId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(mHwnd);
+        var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(winId);
 
-        // var hWndDesktopChildSiteBridge = Win32Helpers.FindWindowEx(mHwnd, IntPtr.Zero, "Microsoft.UI.Content.ContentWindowSiteBridge", null);
+        var hWndDesktopChildSiteBridge =
+            Win32Helpers.FindWindowEx(mHwnd, IntPtr.Zero, "Microsoft.UI.Content.ContentWindowSiteBridge", null);
 
-       // var presenter=appWindow.Presenter as Microsoft.UI.Windowing.OverlappedPresenter;
+        _presenter = appWindow.Presenter as OverlappedPresenter;
+        _presenter.SetBorderAndTitleBar(false, false);
+        _presenter.IsResizable = false;
 
-       // if (presenter != null)
-       // {
-       //     // presenter.IsResizable = true;
-       // }
+        int nValue = (int)Win32Helpers.DWM_WINDOW_CORNER_PREFERENCE.DWMWCP_DEFAULT;
+        Win32Helpers.DwmSetWindowAttribute(mHwnd, (int)Win32Helpers.DWMWINDOWATTRIBUTE.DWMWA_WINDOW_CORNER_PREFERENCE,
+            ref nValue, Marshal.SizeOf(typeof(int)));
 
-       SwapChainCanvas.Loaded += SwapChainCanvas_Loaded;
+        SwapChainCanvas.Loaded += SwapChainCanvas_Loaded;
 
         SizeChanged += Window_SizeChanged;
 
@@ -67,29 +67,67 @@ public sealed partial class MainWindow
         timer.Tick += Timer_Tick;
         timer.Interval = TimeSpan.FromMilliseconds(16D);
         DirectXHelper.InitDirectX();
+        long nExStyle = Win32Helpers.GetWindowLong(mHwnd, Win32Helpers.GWL_EXSTYLE);
 
 
+        Win32Helpers.SetWindowLong(mHwnd, Win32Helpers.GWL_EXSTYLE, (IntPtr)(nExStyle | Win32Helpers.WS_EX_LAYERED));
+        // Win32Helpers.SetWindowLong(mHwnd, Win32Helpers.GWL_EXSTYLE, 0);
+
+        // SetClickThrough();
+    }
+
+    private void SetWorkerWParent()
+    {
+        var hShellViewWin = IntPtr.Zero;
+        var hWorkerW = IntPtr.Zero;
+
+        var hProgman = Win32Helpers.FindWindow("Progman", "Program Manager");
+        var hDesktopWnd = Win32Helpers.GetDesktopWindow();
+
+
+
+        if (hProgman != IntPtr.Zero)
+        {
+            // Get and load the main List view window containing the icons.
+            hShellViewWin = Win32Helpers.FindWindowEx(hProgman, IntPtr.Zero, "SHELLDLL_DefView", null);
+            if (hShellViewWin == IntPtr.Zero)
+            {
+                // When this fails (picture rotation is turned ON), then look for the WorkerW windows list to get the
+                // correct desktop list handle.
+                // As there can be multiple WorkerW windows, iterate through all to get the correct one
+                do
+                {
+                    hWorkerW = Win32Helpers.FindWindowEx(hDesktopWnd, hWorkerW, "WorkerW", null);
+                    hShellViewWin = Win32Helpers.FindWindowEx(hWorkerW, IntPtr.Zero, "SHELLDLL_DefView", null);
+                } while (hShellViewWin == IntPtr.Zero && hWorkerW != IntPtr.Zero);
+            }
+
+            Win32Helpers.SetParent(mHwnd, hShellViewWin);
+        }
+
+        // var progman = Win32Helpers.FindWindow("Progman", null);
         //
-   
+        // IntPtr shell = Win32Helpers.FindWindowEx(progman, IntPtr.Zero, "SHELLDLL_DefView", null);
+        //
+        // IntPtr workerw = IntPtr.Zero;
+        // workerw = Win32Helpers.FindWindowEx(IntPtr.Zero, workerw, "WorkerW", null);
+        // if (shell != IntPtr.Zero)
+        // {
+        //     // Win32Helpers.SetParent(mHwnd, shell);
+        // }
     }
 
     private void Window_SizeChanged(object sender, WindowSizeChangedEventArgs args)
     {
-        SwapChainCanvas.Width= args.Size.Width;
+        SwapChainCanvas.Width = args.Size.Width;
         SwapChainCanvas.Height = args.Size.Height;
         Debug.WriteLine($"Window_SizeChanged {args.Size.Width} {args.Size.Height}");
-        DirectXHelper.ResizeSwapChain((int)args.Size.Width,(int)args.Size.Height);
+        DirectXHelper.ResizeSwapChain((int)args.Size.Width, (int)args.Size.Height);
     }
 
 
-
-
-
- 
-
     private void SwapChainCanvas_Loaded(object sender, RoutedEventArgs e)
     {
-
         SwapChainCanvas.Width = AppWindow.Size.Width;
         SwapChainCanvas.Height = AppWindow.Size.Height;
 
@@ -98,9 +136,7 @@ public sealed partial class MainWindow
         timer.Start();
     }
 
-  
 
-   
     private void Timer_Tick(object sender, object e)
     {
         Draw();
@@ -122,17 +158,49 @@ public sealed partial class MainWindow
 
     private void ToggleTitle(object sender, RoutedEventArgs e)
     {
-        //remove border
-        long nExStyle = Win32Helpers.GetWindowLong(mHwnd, Win32Helpers.GWL_EXSTYLE);
-        // if ((nExStyle & Win32Helpers.WS_EX_LAYERED) == 0)
-        // {
-        //     Win32Helpers.SetWindowLong(mHwnd, Win32Helpers.GWL_EXSTYLE, (IntPtr)(nExStyle | Win32Helpers.WS_EX_LAYERED));
-        // }
-       Win32Helpers.SetWindowLong(mHwnd, Win32Helpers.GWL_EXSTYLE, (IntPtr)(nExStyle & ~Win32Helpers.WS_EX_LAYERED));
-       Win32Helpers.RedrawWindow(mHwnd, IntPtr.Zero, IntPtr.Zero, Win32Helpers.RDW_ERASE | Win32Helpers.RDW_INVALIDATE | Win32Helpers.RDW_FRAME | Win32Helpers.RDW_ALLCHILDREN);
+        if (ExtendsContentIntoTitleBar != true)
+        {
+            ExtendsContentIntoTitleBar = true;
+            _presenter.SetBorderAndTitleBar(true, true);
+            // SetClickThrough();
+        }
+        else
+        {
+            ExtendsContentIntoTitleBar = false;
+            _presenter.SetBorderAndTitleBar(false, false);
+            _presenter.IsResizable = false;
+        }
+    }
 
-        nExStyle = Win32Helpers.GetWindowLong(mHwnd, Win32Helpers.GWL_EXSTYLE);
+    private void SetClickThrough()
+    {
+        int nAppsUseLightTheme = 0;
+        int nSystemUsesLightTheme = 0;
+        string sPathKey = @"Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize";
+        using (RegistryKey rkLocal = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry64))
+        {
+            using (RegistryKey rk = rkLocal.OpenSubKey(sPathKey, false))
+            {
+                nAppsUseLightTheme = (int)rk.GetValue("AppsUseLightTheme", 0);
+                nSystemUsesLightTheme = (int)rk.GetValue("SystemUsesLightTheme", 0);
+            }
+        }
 
-        Win32Helpers.SetWindowLong(mHwnd, Win32Helpers.GWL_EXSTYLE, (IntPtr)(nExStyle | Win32Helpers.WS_EX_LAYERED));
+        uint nColorBackground = (uint)System.Drawing.ColorTranslator.ToWin32(System.Drawing.Color.Black);
+        //if (nAppsUseLightTheme == 1 || nSystemUsesLightTheme == 1)
+        if (nAppsUseLightTheme == 1)
+        {
+            nColorBackground = (uint)System.Drawing.ColorTranslator.ToWin32(System.Drawing.Color.White);
+            // not refreshed when mouse over...
+            // myButton.Foreground = new SolidColorBrush(Microsoft.UI.Colors.White);
+        }
+
+        // set clickthrough
+        Win32Helpers.SetLayeredWindowAttributes(mHwnd, nColorBackground, 255, Win32Helpers.LWA_COLORKEY);
+    }
+
+    private void SetWallPaper(object sender, RoutedEventArgs e)
+    {
+        SetWorkerWParent();
     }
 }
